1ffd67152539e4f55dee1e83e51ec858ce41ecfb
johnmiller16 <johnmiller16@OLINLAB204-18A.augustana.edu>
2018-09-21 14:40:50 -0500

    Code to create the main window which plays the video. The code I added gets the video into the window and you can change the frame from the slider but we still need to add code to get the video to play on it's own.

--- ProjectAvocet/src/application/MainWindowController.java
+++ ProjectAvocet/src/application/MainWindowController.java
@@ -1,5 +1,67 @@
 package application;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
 public class MainWindowController {
-	
+	@FXML
+	private ImageView myImageView;
+	@FXML
+	private Slider sliderSeekBar;
+	private VideoCapture video = new VideoCapture();
+
+	@FXML
+	public void handleBrowse() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Video File");
+		Window mainWindow = myImageView.getScene().getWindow();
+		File chosenFile = fileChooser.showOpenDialog(mainWindow);
+		if (chosenFile != null) {
+			video.open(chosenFile.getAbsolutePath());
+			sliderSeekBar.setMax(video.get(Videoio.CV_CAP_PROP_FRAME_COUNT)-1);
+			displayFrame();
+		} 
+
+	}
+
+	@FXML 
+	protected void handleSlider() {
+		sliderSeekBar.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+				if (sliderSeekBar.isValueChanging()) {
+					video.set(Videoio.CAP_PROP_POS_FRAMES, arg2.intValue());
+					displayFrame();
+				}
+			}
+		});
+	}
+
+	protected void displayFrame() {
+		Mat frame = new Mat();
+		video.read(frame);
+		MatOfByte buffer = new MatOfByte();
+		Imgcodecs.imencode(".png", frame, buffer);
+		Image currentFrameImage = new Image(new ByteArrayInputStream(buffer.toArray()));
+		Platform.runLater(new Runnable() {
+			public void run() {
+				myImageView.setImage(currentFrameImage);
+			}
+		});
+	}
 }
