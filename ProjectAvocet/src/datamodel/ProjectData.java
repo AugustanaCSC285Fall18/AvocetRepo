1ea8c7b7e4676114406d8acf07741609c6d3b9a4
johnmiller16 <johnmiller16@OLINLAB204-18A.augustana.edu>
2018-09-29 14:06:15 -0500

    More changes to try to get auto track working, we should try to get the video playing by itself before we test the autotracker more

--- ProjectAvocet/src/datamodel/ProjectData.java
+++ ProjectAvocet/src/datamodel/ProjectData.java
@@ -1,9 +1,30 @@
 package datamodel;
 
 import java.util.List;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class ProjectData {
 	private List<AnimalTrack> tracks;
 	private Video video;
+	private List<AnimalTrack> unassignedSegments;
+
+	public ProjectData(String videoFilePath) throws FileNotFoundException {
+		video = new Video(videoFilePath);
+		tracks = new ArrayList<>();
+		unassignedSegments = new ArrayList<>();
+	}
+	
+	public Video getVideo() {
+		return video;
+	}
+	
+	public List<AnimalTrack> getTracks(){
+		return tracks;
+	}
+	
+	public List<AnimalTrack> getUnassignedSegments() {
+		return unassignedSegments;
+	}
 
 }
