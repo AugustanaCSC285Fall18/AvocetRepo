cb08c30d1315d256ab2a8ec88847bbe74d114222
Paul Cabrera <paulcabrera17@augustana.edugit config user.email paulcabrera17@augustana.edu>
2018-10-19 13:52:37 -0500

    Added the Gson stuff with fixed Video Class with TUAN.

--- ProjectAvocet/src/datamodel/ProjectData.java
+++ ProjectAvocet/src/datamodel/ProjectData.java
@@ -1,10 +1,16 @@
 package datamodel;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import csv.CSVUtils;
 import javafx.stage.Stage;
@@ -56,5 +62,29 @@ public class ProjectData {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void saveToFile(File saveFile) throws FileNotFoundException {
+		String json = toJSON();
+		PrintWriter out = new PrintWriter(saveFile);
+		out.print(json);
+		out.close();
+	}
+	
+	public String toJSON() {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();		
+		return gson.toJson(this);
+	}
+	
+	public static ProjectData loadFromFile(File loadFile) throws FileNotFoundException {
+		String json = new Scanner(loadFile).useDelimiter("\\Z").next();
+		return fromJSON(json);
+	}
+	
+	public static ProjectData fromJSON(String jsonText) throws FileNotFoundException {
+		Gson gson = new Gson();
+		ProjectData data = gson.fromJson(jsonText, ProjectData.class);
+		data.getVideo().connectVideoCapture();
+		return data;
 	}
 }
